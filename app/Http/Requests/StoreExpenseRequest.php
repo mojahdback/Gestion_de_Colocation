fbe859c5ad8ef8ae2fcd799e4906e9a976a0df9b
mohamed <mojahdontheway@gmail.com>
2026-03-04 14:43:33 +0100

    add validation for expense

---
+++ app/Http/Requests/StoreExpenseRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreExpenseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+        'title' => 'required|string|max:255',
+        'amount' => 'required|numeric|min:0',
+        'date' => 'required|date',
+        'payer_id' => 'required|exists:users,id',
+        'category_id' => 'required|exists:categories,id',
+        ];
+    }
+}
